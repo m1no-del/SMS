d332eb1d12973b9c4db2e4e6551063e741137d1b
m1no-del <cocoromeo20@gmail.com>
2025-03-26 14:32:10 +0800

    added programs

---
+++ SMS.Domain/programs.cs
@@ -0,0 +1,10 @@
+﻿namespace SMS.Domain
+{
+    public class programs
+    {
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
