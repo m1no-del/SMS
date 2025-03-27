6bf0a683fc6ce148a4e5ad71ef359defebef2f02
m1no-del <cocoromeo20@gmail.com>
2025-03-27 13:41:34 +0800

    recorrect programs to Programs

--- SMS.Domain/programs.cs
+++ SMS.Domain/programs.cs
@@ -2,11 +2,11 @@
 
 namespace SMS.Domain
 {
-    public class programs
+    public class Programs
     {
         [Key]
         public int ProgramId { get; set; }
-        public string ProgramName { get; set; }
+        public string? ProgramName { get; set; }
         public string? Description { get; set; }
 
     }
